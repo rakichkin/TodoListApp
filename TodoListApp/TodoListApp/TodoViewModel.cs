0f6080f37d09dfcf7cc544d479a6e92fac9bc8fc
rakichkin <rakichkin@gmail.com>
2022-02-01 01:47:52 +0300

    Добавьте файлы проекта.

---
+++ TodoListApp/TodoListApp/TodoViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TodoListApp
+{
+    public class TodoViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem("abc", false));
+            TodoItems.Add(new TodoItem("abc", true));
+            TodoItems.Add(new TodoItem("abc", false));
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoInputValue { get; set; }
+        public void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+        }
+
+        public ICommand RemoveTodoCommand => new Command(DeleteTodoItem);
+        public void DeleteTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+    }
+}
